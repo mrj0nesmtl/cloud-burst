9cddbc213f4e706422a610cbbf9f4abe3f44ba46
Joel <joel.yaffe@gmail.com>
2025-02-24 18:55:09 -0500

    chore: update project configuration for beta deployment

--- replit.nix
+++ replit.nix
@@ -1,13 +1,10 @@
 nix:replit.nix
 { pkgs }: {
     deps = [
-        pkgs.nodejs-18_x
+        pkgs.nodejs-20_x
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.typescript
-        pkgs.yarn
-        pkgs.replitPackages.jest
         pkgs.git
-        pkgs.openssh
     ];
     env = {
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
