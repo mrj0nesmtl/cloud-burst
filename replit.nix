ab84e337e5d86e71aaa42b44186408038b0b916b
Joel <joel.yaffe@gmail.com>
2025-02-24 04:00:57 +0000

    fix(deps): update replit.nix to use nodejs-18_x  - Switch from nodejs-20_x to nodejs-18_x for compatibility - Fix Node environment configuration

--- replit.nix
+++ replit.nix
@@ -1,14 +1,14 @@
 { pkgs }: {
-    deps = [
-        pkgs.nodejs-20_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.git
-        pkgs.python3
-        pkgs.gcc
+  deps = [
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.git
+    pkgs.python3
+    pkgs.gcc
+  ];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libuuid
     ];
-    env = {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.libuuid
-        ];
-    };
-} 
+  };
+}
